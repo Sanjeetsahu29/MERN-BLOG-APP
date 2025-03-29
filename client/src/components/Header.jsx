5b29a5f2d2609dd6ad5143c409b429fbac81e6ef
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2025-03-29 14:48:01 +0530

    create Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,53 @@
+import { Navbar, TextInput, Button, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import {Link, useLocation} from 'react-router-dom'
+import { CiSearch } from "react-icons/ci";
+import { MdDarkMode } from "react-icons/md";
+
 const Header = () => {
+    const path = useLocation().pathname;
   return (
-    <div>Header</div>
+    <Navbar className="border-b-2 border-gray-300">
+        <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 to-pink-500 hover:bg-gradient-to-l hover:from-purple-500 hover:to-lime-200 focus:ring-lime-200 rounded-md tracking-[0.7px] text-white">Sanjeet's</span>
+            Blog
+        </Link>
+        <form>
+            <TextInput
+                type="text"
+                placeholder="Search..."
+                rightIcon={CiSearch}
+                className="hidden lg:inline"
+            />
+        </form>
+        <Button className="text-xl bg-gray-200 lg:hidden hover:bg-gray-300" pill>
+            <CiSearch className="text-black"/>
+        </Button>
+
+        <div className="flex gap-2 md:order-2">
+            <Button className="text-xl bg-gray-200 sm:inline hidden hover:bg-gray-300" pill>
+                <MdDarkMode className="text-black"/>
+            </Button>
+            <Link to="/sign-in">
+                <Button  className="bg-gradient-to-r from-teal-200 to-lime-200 text-gray-900 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-lime-200 cursor-pointer">
+                    Sign In
+                </Button>
+            </Link>
+            <NavbarToggle/>
+        </div>
+        
+        <NavbarCollapse>
+            <NavbarLink active={path === "/"} as={'div'}>
+                <Link to='/'>Home</Link>
+            </NavbarLink>
+            <NavbarLink active={path === "/about"} as={'div'}>
+                <Link to='/about'>About</Link>
+            </NavbarLink>
+            <NavbarLink active={path === "/projects"} as={'div'}>
+                <Link to='/projects'>Projects</Link>
+            </NavbarLink>
+        </NavbarCollapse>
+    </Navbar>
   )
 }
 
-export default Header
+export default Header  
