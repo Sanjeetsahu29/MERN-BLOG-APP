856bb52588d247012fdd92243d9bc29f31d235e2
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2025-03-29 15:31:59 +0530

    bug solved

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import mongoose from'mongoose';
 import dotenv from 'dotenv';
-
 dotenv.config();
 const app = express();
 mongoose.connect(
-    process.env.MONGODB_URI,
+    process.env.MONGO_URI
 ).then(() => console.log('MongoDB Connected...'))
  .catch(err => console.log(err));
-app.listen(5000, async() =>{
-    console.log('Server is running on port 5000')
+const PORT = process.env.PORT || 2000;
+app.listen(process.env.PORT, async() =>{
+    console.log(`Server is running on port:${PORT}`);
 });
