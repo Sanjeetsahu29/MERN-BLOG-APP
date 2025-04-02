e3e10f6d8221f5699667b601f248a47686ff267e
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2025-04-02 10:35:14 +0530

    add functionality to signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,43 @@
-import {Link} from 'react-router-dom'
+import { useState } from 'react';
+import {Link, useNavigate} from 'react-router-dom'
+import {Spinner} from 'flowbite-react'
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const handleChange=(e)=>{
+      setFormData({...formData, [e.target.id]:e.target.value.trim()})   
+  }
+  // console.log(formData);
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    if(!formData.username || !formData.email || !formData.password){
+      return setError('Please fill all the fields and then try signing up');
+    }
+    try{
+      setLoading(true);
+      setError(null);
+      const response = await fetch('/api/auth/signup', {
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify(formData)
+      })
+      const data = await response.json();
+      if(data.success === false){
+        setLoading(false);
+        return setError(data.message);
+      }
+      console.log(data);
+      setLoading(false);
+      if(response.ok){
+        return navigate('/signin');
+      }
+    }catch(err){
+      setError(err.message);
+      setLoading(false);
+    }
+  }
   return (
     <div className="min-h-screen mt-20 p-6">
       <div className="flex max-w-6xl gap-5 mx-auto flex-col md:flex-row ">
@@ -18,7 +56,7 @@ const SignUp = () => {
         </div>
         <div className="flex-1 bg-slate-100 p-4 rounded-lg">
           {/*right*/}
-          <form className="flex flex-col gap-2">
+          <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
               <h1 className="text-4xl tracking-[1px] font-semibold my-4 ">Sign Up</h1>
               <div className="flex flex-col gap-1">
                 <label htmlFor="username">User name</label>
@@ -27,6 +65,7 @@ const SignUp = () => {
                   placeholder="Your username"
                   id="username"
                   className="block w-full rounded-md border-1 border-gray-300 px-2 py-1.5"
+                  onChange={handleChange}
                 />
               </div>
               <div className="flex flex-col gap-1">
@@ -36,6 +75,7 @@ const SignUp = () => {
                     placeholder="Your email address"
                     id="email"
                     className="block w-full rounded-md border-1 border-gray-300 px-2 py-1.5"
+                    onChange={handleChange}
                   />
               </div>
               <div className="flex flex-col gap-1">
@@ -45,17 +85,26 @@ const SignUp = () => {
                     placeholder="Your password"
                     id="password"
                     className="block w-full rounded-md border-1 border-gray-300 px-2 py-1.5"
+                    onChange={handleChange}
                   />
               </div>
-              <button className="bg-blue-400 hover:bg-blue-500 cursor-pointer py-2 rounded-lg" type="submit">Sign Up</button>
+              <button className="bg-blue-400 hover:bg-blue-500 cursor-pointer py-2 rounded-lg uppercase text-white font-semibold tracking-[1px] " disabled={loading} type="submit">
+                {loading ? 
+                  (<Spinner aria-label="Alternate spinner button example" size="sm" />) 
+                  : 'Sign Up'}
+              </button>
           </form>
           <div className='flex gap-3 text-sm mt-2'>
             <span>Have an account?</span>
             <Link to="/sign-in">
-              <span className="text-blue-500">Sign In</span>
+              <span className="text-blue-500 cursor-pointer hover:underline ">Sign In</span>
             </Link>
           </div>
-
+          {error && 
+            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded-lg relative mt-2" role="alert">
+              <span className="block sm:inline">{error}</span>
+            </div>
+          }
         </div>
       </div>
     </div>
