7d60bf267f2c453ed92876001e9b15e429dcf8ba
Sanjeet Sahu <sanjeetsahus29@gmail.com>
2025-04-02 11:43:41 +0530

    create a signin route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import {errorHandler} from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 export const signup = async (req, res, next)=>{
     const {username, email, password} = req.body;
     if(!username || !email || !password || username.trim() === '' || email.trim() === '' || password.trim() === ''){
@@ -17,5 +18,32 @@ export const signup = async (req, res, next)=>{
         res.status(201).json({msg: 'User registered successfully',user});
     }catch(err){
         return next(err)
+    }
+}
+
+export const signin = async (req, res, next)=>{
+    const {email, password} = req.body;
+    if(!email || !password || email.trim() === '' || password.trim() === ''){
+        return next(errorHandler(400, "All fields are required"));
+    }
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return next(errorHandler(400, "Invalid ceredentials"));
+        }
+        const isPasswordCorrect = bcryptjs.compareSync(password, user.password);
+        if(!isPasswordCorrect){
+            return next(errorHandler(400, "Invalid ceredentials")); 
+        }
+        const token = jwt.sign({
+            id:user._id,
+        },process.env.JWT_SECRET,{expiresIn:'1d'});
+
+        const {password: pass, ...others} = user._doc;
+        res.status(200).cookie('access_token',token,{
+            httpOnly:true
+        }).json(others);        
+    }catch(err){
+        return next(err)    
     }
 }
